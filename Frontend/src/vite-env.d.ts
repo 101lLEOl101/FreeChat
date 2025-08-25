46ad34112e46fae9489bd0ab984291f74520d180
lenyakuchuk <Lenya.kuchuk@gmail.com>
2025-08-25 13:19:33 +0300

    remove containers names to ip in url

--- Frontend/src/vite-env.d.ts
+++ Frontend/src/vite-env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-    readonly VITE_API_URL: string
-    // добавь другие переменные окружения здесь
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv
-}
